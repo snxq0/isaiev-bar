ff7226a295c6539ca284389addb4d80e70e4033c
isnikx0 <isaievnikitaa@gmail.com>
2025-12-09 16:31:00 +0100

    изменил страницу меню

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,21 +1,27 @@
-const buttons = document.querySelectorAll('.category-btn');
-const cards = document.querySelectorAll('.menu-card');
-
-function filterMenu(category) {
-  cards.forEach(card => {
-    card.classList.remove('show');
-    if(category === 'all' || card.classList.contains(category)) {
-      card.classList.add('show');
-    }
-  });
-}
-
-filterMenu('all');
+const buttons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".menu-card");
 
 buttons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    buttons.forEach(b => b.classList.remove('active'));
-    btn.classList.add('active');
-    filterMenu(btn.dataset.category);
-  });
+    btn.addEventListener("click", () => {
+
+        // активная кнопка
+        buttons.forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const category = btn.dataset.category;
+
+        cards.forEach(card => {
+            // фильтрация
+            if (category === "all" || card.classList.contains(category)) {
+                card.classList.add("show");
+            } else {
+                card.classList.remove("show");
+            }
+        });
+    });
+});
+
+// Показ всех карточек при загрузке
+window.addEventListener("load", () => {
+    cards.forEach(card => card.classList.add("show"));
 });
